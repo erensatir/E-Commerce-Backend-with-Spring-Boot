7c4dfaffb3fa9919e67fa18ec3af48970a1dd573
Eren Satir <eren.satir@tokeninc.com>
2023-11-30 19:48:42 +0300

    Starter Code

---
+++ src/main/java/com/example/cs393backend/Cs393backendApplication.java
@@ -0,0 +1,13 @@
+package com.example.cs393backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs393backendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs393backendApplication.class, args);
+	}
+
+}
